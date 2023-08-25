2be9be67b41d0aa3e195f4d7ab60da4a97292abb
GabrielGSantos <bielsantos623@gmail.com>
2023-08-24 22:24:08 -0300

    Listagem de candidatos selecionados.

--- src/ProcessoSeletivo.java
+++ src/ProcessoSeletivo.java
@@ -9,6 +9,15 @@ public class ProcessoSeletivo {
         analisarCandidato(2000.0);
     }
 
+    static void imprimirSelecionados() {
+        String [] candidatos = {"Felipe", "Marcia", "Julia", "Paulo", "Augusto"};
+        System.out.println("Imprimindo a lista de candidatos informando o indice do elemento");
+
+        for(int indice = 0; indice < candidatos.length; indice++) {
+            System.out.println("O candidato de nº" + indice + " é " + candidatos[indice]);
+        }
+    }
+
     static void selecaoCandidatos() {
         String [] candidatos = {"Felipe", "Marcia", "Julia", "Paulo", "Augusto", "Monica", "Fabricio", "Mirela", "Daniela"};
 
